c8e3026a2ae5aaa6a8ff4a793bde7059d1f627e0
RobertSokman <robert.sokman@gmail.com>
2021-02-26 14:36:50 +0000

    commit on 'mainä

---
+++ src/screens/Reminders/Reminders.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { View, Text } from "react-native";
+import FooterTabsIconExample from "../../elements/Template";
+
+const Reminders = (props) => {
+  return (
+    <View>
+      <FooterTabsIconExample />
+      <Text>notJust a component</Text>
+    </View>
+  )
+}
+
+export default Reminders;
+
+//functional component
