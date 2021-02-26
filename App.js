fbe3b0583d675d74e5e274dbf813c169223a8259
RobertSokman <robert.sokman@gmail.com>
2021-02-26 11:20:55 +0000

    commit to 'main'

--- App.js
+++ App.js
@@ -1,14 +1,39 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+
+import React, { Component } from 'react';
+import { StyleSheet, Text, View, Image } from 'react-native';
+import { Container, Header, Content, Footer, FooterTab, Button, Icon } from 'native-base';
+export default class FooterTabsIconExample extends Component {
+  render() {
+    return (
+      <Container>
+        <Header>
+          <Image
+          style={styles.logo}
+          source={require('./assets/Logo.png')}
+          />
+          
+        </Header>
+        <Content />
+        <Footer>
+          <FooterTab>
+            <Button>
+              <Icon name="ios-people-outline" />
+            </Button>
+            <Button>
+              <Icon name="ios-bar-chart-outline" />
+            </Button>
+            <Button active>
+              <Icon active name="alarm-outline" />
+            </Button>
+            <Button>
+              <Icon name="settings-outline" />
+            </Button>
+          </FooterTab>
+        </Footer>
+      </Container>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
@@ -16,6 +41,18 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
+    //justifyContent: 'center',
+  },
+  logo: {
+    width: 180,
+    height: 80,
+    marginTop: -15,
     justifyContent: 'center',
+    //flex: 'flex-start',
+  },
+  header: {
+    //
+    //
+    //
   },
 });
