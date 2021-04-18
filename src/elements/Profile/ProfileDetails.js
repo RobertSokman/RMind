d76dfd69844d826918a216b26b38cedd29f8515a
RobertSokman <robert.sokman@gmail.com>
2021-04-18 15:19:49 +0100

    adding risk level for clients in profile screen

--- src/elements/Profile/ProfileDetails.js
+++ src/elements/Profile/ProfileDetails.js
@@ -8,21 +8,36 @@ const windowWidth = Dimensions.get('window').width;
 
 
 const ProfileDetails = ({name, country, birthday, sector, language, currency}) => {
-  return (
+  if (name == "Travers Millard" || name == "Dupuis Louis") {
+    return (
     
-    <View style={{alignItems: "center", marginTop: windowHeight/8}}>
-        <Icon name="person-circle-outline" style={{fontSize: 160, color: "gray"}} /> 
-        <Text style={{fontWeight: "bold"}}>{name}</Text>
-        <Text>{country}</Text>
-        <Text>Birthday: {birthday}</Text>
-        <Text>{sector} sector</Text>
-        <Text>Language: {language}</Text>
-        <Text>Reporting Currency: {currency}</Text>
-    </View>
+    
+      <View style={{alignItems: "center", marginTop: windowHeight/8}}>
+          <Icon name="person-circle-outline" style={{fontSize: 160, color: 'green'}} /> 
+          <Text style={{fontWeight: "bold" }}>{name}</Text>
+          <Text>{country}</Text>
+          <Text>Birthday: {birthday}</Text>
+          <Text>{sector} sector</Text>
+          <Text>Language: {language}</Text>
+          <Text>Reporting Currency: {currency}</Text>
+      </View>    
       
-   
-    
-  )
+    )
+  } else{
+    return (    
+      <View style={{alignItems: "center", marginTop: windowHeight/8}}>
+          <Icon name="person-circle-outline" style={{fontSize: 160, color: 'gray'}} /> 
+          <Text style={{fontWeight: "bold" }}>{name}</Text>
+          <Text>{country}</Text>
+          <Text>Birthday: {birthday}</Text>
+          <Text>{sector} sector</Text>
+          <Text>Language: {language}</Text>
+          <Text>Reporting Currency: {currency}</Text>
+      </View>
+
+    )
+  }
+  
 }
 
 export default ProfileDetails;
