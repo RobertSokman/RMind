7c9ed633531c11b1fe5c70a9b270ac1199f4cac5
RobertSokman <robert.sokman@gmail.com>
2021-04-26 19:34:17 +0100

    client list added buttons

--- src/screens/Client list/ClientListItem.js
+++ src/screens/Client list/ClientListItem.js
@@ -1,11 +1,12 @@
 import React, {useEffect, useState} from "react";
-import { View, Text, FlatList, Dimensions, Pressable } from "react-native";
+import { View, Text, FlatList, Dimensions, Pressable, TextInput } from "react-native";
 const windowHeight = Dimensions.get('window').height;
 const windowWidth = Dimensions.get('window').width;
 import {useNavigation} from '@react-navigation/native';
 import ClientName from "../../elements/ClientList/Clients";
 import { API, graphqlOperation } from "aws-amplify";
 import { listClients } from "../../graphql/queries";
+import { Button } from "native-base";
 
 
 const ClientListItem = (props) => {
@@ -33,12 +34,21 @@ const ClientListItem = (props) => {
                 data={clients}
                 renderItem={({item}) => 
                 
+                <View style={{flexDirection: 'row', justifyContent: 'space-between'}}>
+                    
+                <ClientName client={item} />
+                
                 <Pressable onPress={() => navigation.navigate("Profile", {name: item.name, surname: item.surname, country: item.nationality,
                                                                                birthday: item.birthday, sector: item.sector, language: item.languageOfReporting, 
                                                                             currency: item.reportingCurrency, id: item.id, customerNo: item.customerNo })}
+                style={{borderWidth: 2, width: windowWidth/5, borderColor:'#003662',
+                    height: windowHeight/25, borderRadius: 10, backgroundColor: '#003f5c', alignSelf:'center', marginRight: 10}}
                 >
-                    <ClientName client={item} />
+                    
+                    <Text style={{textAlign: 'center', color: 'white', marginTop: 5}}>View</Text>
+                    
                 </Pressable>
+                </View>
                 }
                 
             />
